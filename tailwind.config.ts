2a13b926db78c4c943d81c31c1533a50b3b75f74
0xBenjamintan <ben@blonkfi.com>
2024-08-24 06:00:35 +0900

    changed theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["halloween"],
+    themes: ["dracula"],
     darkTheme: "dark",
     base: true,
     styled: true,
