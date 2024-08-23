956c52ad12f219183fb4cc424c08fda968684000
cheongyeechian <cheongyeechian@gmail.com>
2024-08-24 05:30:17 +0900

    feat: Connect Wallet function done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { ThirdwebProvider } from "thirdweb/react";
 
 const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "FlappyQF",
@@ -19,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ThirdwebProvider>
+          <Header />
+          {children}
+        </ThirdwebProvider>
       </body>
     </html>
   );
