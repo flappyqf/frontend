df47b8e61ce3f7390fa25fcbd07478ccdc2c2432
cheongyeechian <cheongyeechian@gmail.com>
2024-08-24 05:30:17 +0900

    feat: Connect Wallet function done

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,15 +1,43 @@
 "use client";
-import React, { useState } from "react";
+import { createThirdwebClient, defineChain } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
+import {
+  inAppWallet,
+  createWallet,
+} from "thirdweb/wallets";
+import { scrollSepoliaTestnet } from "thirdweb/chains";
+
 
 const Header = () => {
+  const client = createThirdwebClient({ clientId: process.env.NEXT_PUBLIC_CLIENT_ID || "" });
+  const chain = defineChain(534351);
+  const wallets = [
+  inAppWallet({
+    auth: {
+      options: [
+        "google",
+        "email",
+        "passkey",
+      ],
+    },
+  }),
+
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow"),
+  createWallet("io.rabby"),
+  createWallet("io.zerion.wallet"),
+];
+
   return (
-    <header className="mx-auto py-8 lg:py-6 bg-neutral sticky top-0 z-50">
-      <div className="container mx-auto px-8 lg:px-4 flex items-center justify-between">
-        <div className="flex items-center">
-          <h1 className="text-2xl font-bold uppercase text-primary">
-            FlappyQF
-          </h1>
-        </div>
+ 
+      <header className="mx-auto py-8 lg:py-6 bg-neutral sticky top-0 z-50">
+        <div className="container mx-auto px-8 lg:px-4 flex items-center justify-between">
+          <div className="flex items-center">
+            <h1 className="text-2xl font-bold uppercase text-primary">
+              FlappyQF
+            </h1>
+          </div>
         <nav className="hidden md:flex gap-4 mx-auto">
           <a
             href="/"
@@ -34,12 +62,23 @@ const Header = () => {
             </a>
           </div>
         </footer>
-        {/* Put connect button here */}
-        <button className="border rounded-md py-2 px-4 hover:bg-black hover:text-white transition-all duration-300">
-          Test
-        </button>
-      </div>
-    </header>
+          {/* Put connect button here */}
+          <div>
+            <ConnectButton
+              client={client}
+              wallets={wallets}
+              connectButton={{ label: "Connect Wallet" }}
+              connectModal={{ size: "wide" }}
+              accountAbstraction={{
+                chain, 
+                factoryAddress: "0xCE7b85ADc9913A31eC7610A83e38b7D7eAA18747",
+                sponsorGas: true,
+              }}
+            />
+          </div>
+        </div>
+      </header>
+
   );
 };
 
