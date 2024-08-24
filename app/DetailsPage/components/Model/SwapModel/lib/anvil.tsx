b0e086ef25a5c773fccaf1669f88233718467976
cheongyeechian <cheongyeechian@gmail.com>
2024-08-24 19:41:06 +0900

    Swap and contribute V2

---
+++ app/DetailsPage/components/Model/SwapModel/lib/anvil.tsx
@@ -0,0 +1,12 @@
+import { defineChain } from "thirdweb"
+
+export default defineChain({
+    id: 534351,
+    name: "ETH",
+    testnet: true,
+    nativeCurrency: {
+        name: "QFNY Ether",
+        symbol: "ETH",
+        decimals: 18,
+    },
+});
